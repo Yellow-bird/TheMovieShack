15790d2baae88e5dcde9e62ad7c7fc8f47d2509a
Vyara <vyara_hristova@yahoo.de>
2015-08-31 18:00:48 +0300

    moved libraries to lib folder, added references in index.html

--- app/app.js
+++ app/app.js
@@ -1,8 +1,1 @@
 'use strict';
-
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
