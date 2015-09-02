7ded918fb4949771c539805e1d8f76728cbbd98c
vyara <vyara_hristova@yahoo.de>
2015-09-02 15:43:46 +0300

    successfully adds movie to parse with properties year title and genre

--- app/scripts/controllers/add-movie-details-events.js
+++ app/scripts/controllers/add-movie-details-events.js
@@ -1,5 +1,4 @@
 import {createMovie} from 'scripts/models/movie.js';
-
 export function submitMovieDetails() {
     var $submitButton = $('#submit-movie-details-btn');
     $submitButton.on('click', function () {
@@ -7,9 +6,23 @@ export function submitMovieDetails() {
         var movieYear = $('#select').val();
         var movieGenre = $('#inputGenre').val();
         var movie = createMovie(movieTitle, movieYear, movieGenre);
-        console.log(movie.title);
-        console.log(movie.year);
-        console.log(movie.genre);
+        var Movie = Parse.Object.extend('Movie');
+
+        //can be later moved to a save function
+        var newMovie = new Movie();
+
+        newMovie.set('title', movie.title);
+        newMovie.set('year', movie.year);
+        newMovie.set('genre', movie.genre);
+
+
+        newMovie.save().then(function() {
+            alert("yay! it worked");
+        });
+
+        //console.log(movie.title);
+        //console.log(movie.year);
+        //console.log(movie.genre);
     })
 
 }
