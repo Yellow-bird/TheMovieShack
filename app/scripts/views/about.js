b28c4cf2c82e538eb4c14a0ad85eb87de67a4ea6
BrunoHautenfaust <t1m2n3t4@yahoo.com>
2015-09-04 02:40:05 +0300

    added logo, presentation, edited About page.

--- app/scripts/views/about.js
+++ app/scripts/views/about.js
@@ -2,7 +2,6 @@ export function renderAbout(){
     'use strict';
     var $mainContent = $('#main-content');
 
-    $mainContent.html('About');
-
-    alert('in about');
+    $mainContent.html('<br>This is a Single Page Application Project for movie, TV and celebrity content.');
+    
 }
